fe783ddecb6aed88b8a922719d9722d981b0f728
Anatoliy Lobanov <lovamuzik@yandex.ru>
2022-10-27 23:04:41 +0300

    Изменено количество человек

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> surnames = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     surnames.get(new Random().nextInt(surnames.size())),
